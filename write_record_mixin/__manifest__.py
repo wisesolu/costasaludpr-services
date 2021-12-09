9b0297fb8e7cf7751c7be04b7619fb030759dede
deb-odoo <deb@odoo.com>
2021-12-09 15:15:55 +0530

    [ADD]: Added the module costasaludpr_custom_write_field and write_record_mixin.

---
+++ write_record_mixin/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Custom write record mixin",
+
+    'summary': """
+        This module is having mixin file for adding custom write fields.
+    """,
+
+    'description': """
+        Task id:  2687678
+        This module is having mixin file for adding custom write fields by uid and date.
+        Also write method in mixin is been inherited to fill these fields.
+    """,
+
+    'author': 'Odoo Ps',
+    'version': '1.0.0',
+
+    'installable': True,
+}
