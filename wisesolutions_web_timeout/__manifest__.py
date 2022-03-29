245c165ce6f8c074ec22d682e594ba301c3923ec
Zeno Nanon <zna@odoo.com>
2022-03-29 11:19:24 -0700

    [ADD] auto logout module

---
+++ wisesolutions_web_timeout/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Wise Solutions: Session Timeout",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Task ID: 2480404
+        Long description of module's purpose
+    """,
+
+    'author': "Odoo Inc.",
+    'website': "http://www.odoo.com",
+    'category': 'Customizations/Studio',
+    'license': 'OEEL-1',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        'views/res_config_settings.xml',
+    ],
+}
